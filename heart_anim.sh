0253e438942bf2c751ed8e9f6299c1fa74752d4a
samir2901 <thunder.cse2018@gmail.com>
2020-08-05 22:09:42 +0530

    Heart Ascii Animation

---
+++ heart_anim.sh
@@ -0,0 +1,49 @@
+#! /usr/bin/bash
+
+clear
+while [ 1 ]
+do
+    echo "         OOOOO             OOOOO"
+    sleep $1
+    echo "     OOOO    OOOO       OOOO    OOOO"
+    sleep $1
+    echo "  OOO            OOO OOO            OOO"
+    sleep $1
+    echo " OOOO               O               OOOO"
+    sleep $1
+    echo " OOOO                               OOOO"
+    sleep $1
+    echo " OOOO                               OOOO"
+    sleep $1
+    echo " OOOO                               OOOO"
+    sleep $1
+    echo "  OOO                               OOO"
+    sleep $1
+    echo "   OOO                             OOO"
+    sleep $1
+    echo "    OOO                           OOO"
+    sleep $1
+    echo "     OOO                         OOO"
+    sleep $1
+    echo "      OOO                       OOO"
+    sleep $1
+    echo "       OOO                     OOO"
+    sleep $1
+    echo "         OOO                 OOO"
+    sleep $1
+    echo "           OOO             OOO"
+    sleep $1
+    echo "             OOO         OOO"
+    sleep $1
+    echo "               OOO     OOO"
+    sleep $1
+    echo "                 OO   OO"
+    sleep $1
+    echo "                   OO"
+    sleep $1
+    clear    
+done
+
+
+               
+
